dfe2883af3018e55e9d1cd74e09913e44e4f66a6
Pavel Kirienko <pavel.kirienko@gmail.com>
2023-06-27 15:37:25 +0300

    API rework WIP

--- libudpard/udpard.c
+++ libudpard/udpard.c
@@ -1,8 +1,7 @@
-/// This library is based heavily on libcanard
 /// This software is distributed under the terms of the MIT License.
 /// Copyright (c) 2016 OpenCyphal.
-/// Author: Pavel Kirienko <pavel@opencyphal.org>
 /// Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+/// Author: Pavel Kirienko <pavel@opencyphal.org>
 
 #include "udpard.h"
 #include "_udpard_cavl.h"
