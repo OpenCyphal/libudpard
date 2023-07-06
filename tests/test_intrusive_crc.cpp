a35339c42c0113a776adb1f61781b479fa8f1e9f
Pavel Kirienko <pavel.kirienko@gmail.com>
2023-07-06 22:05:50 +0300

    BROKEN: switch to one-test-per-executable with #include <udpard.c>; not buildable

--- tests/test_intrusive_crc.cpp
+++ tests/test_intrusive_crc.cpp
@@ -3,23 +3,20 @@
 /// Copyright Amazon.com Inc. or its affiliates.
 /// SPDX-License-Identifier: MIT
 
-#include "exposed.hpp"
+#include <udpard.c>  // NOLINT(bugprone-suspicious-include)
 #include <gtest/gtest.h>
 
 TEST(CRC, Header)
 {
-    using exposed::headerCRCCompute;
     ASSERT_EQ(0x29B1U, headerCRCCompute(9, "123456789"));
 }
 
 TEST(CRC, Transfer)
 {
-    using exposed::transferCRCAdd;
-    constexpr std::uint32_t OutputXOR = 0xFFFFFFFFU;
-    auto                    crc       = transferCRCAdd(0xFFFFFFFFU, 3, "123");
-    crc                               = transferCRCAdd(crc, 6, "456789");
+    auto crc = transferCRCAdd(0xFFFFFFFFU, 3, "123");
+    crc      = transferCRCAdd(crc, 6, "456789");
     ASSERT_EQ(0x1CF96D7CUL, crc);
-    ASSERT_EQ(0xE3069283UL, crc ^ OutputXOR);
+    ASSERT_EQ(0xE3069283UL, crc ^ TRANSFER_CRC_OUTPUT_XOR);
     crc = transferCRCAdd(crc,
                          4,
                          "\x83"  // Least significant byte first.
@@ -27,5 +24,5 @@ TEST(CRC, Transfer)
                          "\x06"
                          "\xE3");
     ASSERT_EQ(0xB798B438UL, crc);
-    ASSERT_EQ(0x48674BC7UL, crc ^ OutputXOR);
+    ASSERT_EQ(0x48674BC7UL, crc ^ TRANSFER_CRC_OUTPUT_XOR);
 }
